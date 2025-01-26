ca2f311c1c139942e3a950655dc8c91a92cdc49d
ALBANTA LD <albanta.leondelgado@gmail.com>
2025-01-26 21:04:27 +0000

    First Commit

--- src/models.py
+++ src/models.py
@@ -28,5 +28,39 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    description = Column(String(250))
+    birth_year =Column(String(250))
+    gender = Column(String(250))
+    height = Column(String(250))
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
+    hair_color = Column(String(250))
+    picture_url = Column(String(250))
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    description = Column(String(250))
+    climate = Column(String(250))
+    population = Column(String(250))
+    orbital_period = Column(String(250))
+    rotation_period = Column(String(250))
+    diameter = Column(String(250))
+    terrain = Column(String(250))
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(Character)
+
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
